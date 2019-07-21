7dcc1333cbd49c47cd447af6f3700d35e3cd66de
Joanna Kęprowska <a.keprowska@gmail.com>
2019-07-21 15:39:21 +0200

    Stworzenie w root przycisków i labben

--- src/main/java/pl/sda/weather/controller/RootController.java
+++ src/main/java/pl/sda/weather/controller/RootController.java
@@ -1,5 +1,14 @@
 package pl.sda.weather.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class RootController {
+
+    //tworzymy teraz pole typu textField z controll:
+
+    @FXML
+    private TextField city;//city bo taką nazwę identyfikatora podaliśmy w root.fxml
+
 
 }
