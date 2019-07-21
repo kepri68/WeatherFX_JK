0630ed578b3cd264fd399749e945d0847c9a2359
Joanna Kęprowska <a.keprowska@gmail.com>
2019-07-21 15:23:31 +0200

    Dodanie do naszego projektu pliku root.fxml - stworzonego przez aplikację SceneBuilder - wywołanie pliku (okienko aplikacji) ! Tak tworzymy kod zawsze, kiedy chcemy stworzyć aplikację

--- src/main/java/pl/sda/weather/App.java
+++ src/main/java/pl/sda/weather/App.java
@@ -1,13 +1,33 @@
 package pl.sda.weather;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application{
+
+    public static void main( String[] args ){
+    launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        //załadowanie naszego pliku root.fxml
+        Parent root =
+                FXMLLoader.load(
+                        getClass()
+                                .getResource("/root.fxml"));
+        //^ służy do załadowania widoku - za każdym razem tak samo musimy zrobić
+
+        //teraz na obiekcie Stage musimy wstawić scenę z widokiem xmlowym:
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
